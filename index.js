868f2069678884453c75fe2515956f0244602b10
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-08 20:31:18 -0600

    Javascript Changes

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 //.padEnd()
 
 console.log('Turtle'.padStart(10));
+
+console.log('Turtle'.padEnd(10));
+
+const obj = { a: 1, b: 2, c: 3 };
+const values = Object.values(obj);
+console.log(values); // Output: [1, 2, 3]
