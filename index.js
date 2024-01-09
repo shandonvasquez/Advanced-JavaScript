834b17b73c4ef2d0438af8c8b29eefc634162b1f
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-08 20:53:42 -0600

    Javascript Changes

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ console.log('Turtle'.padEnd(10));
 const obj = { a: 1, b: 2, c: 3 };
 const values = Object.values(obj);
 console.log(values); // Output: [1, 2, 3]
+
+
+let ob1 = {
+    username0: 'Santa',
+    username1: 'Rudolf',
+    username2: 'Mr.Grinch'
+}
+
+Object.keys(ob1).forEach((key, index) => {
+    console.log(key, ob1[key]);
+})
+
+Object.values(ob1).forEach(value => {   
+    console.log(value);
+}
+)
+
+Object.entries(ob1).forEach(value => {
+    console.log(value);
+}
+)
