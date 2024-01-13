9a04ecc730bdc0cead1af8b68752423ce9ea54c0
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-12 22:52:21 -0600

    Javascript Changes

--- index.js
+++ index.js
@@ -24,21 +24,22 @@ console.log(rabbit.padEnd(7));
 // console.log(values); // Output: [1, 2, 3]
 
 
-// let ob1 = {
-//     username0: 'Santa',
-//     username1: 'Rudolf',
-//     username2: 'Mr.Grinch'
-// }
+let ob1 = {
+    username0: 'Santa',
+    username1: 'Rudolf',
+    username2: 'Mr.Grinch'
+}
 
-// Object.keys(ob1).forEach((key, index) => {
-//     console.log(key, ob1[key]);
-// })
+Object.keys(ob1).forEach((key, index) => {
+    console.log(key, ob1[key]);
+})
 
-Object.values(ob1).forEach(value => {   
+Object.values(ob1).forEach(value => {
     console.log(value);
 }
 )
 
- Object.entries(ob1).forEach(value => {
-    console.log(value); }
- )
+Object.entries(ob1).forEach(value => {
+    console.log(value);
+}
+)
