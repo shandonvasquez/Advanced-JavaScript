64f787c275d3d3f1d656172d92930799d6f29ca5
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-20 18:21:03 -0600

    Javascript Changes

--- index.js
+++ index.js
@@ -69,3 +69,29 @@ console.log(array.flat(2));
 
 // #2 Turn this array into a new array: [ 'Hello young grasshopper!', 'you are', 'learning fast!' ]
 const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+
+console.log(greeting.flatMap(value => value.join(' ')));
+// Solve the below questions:
+
+
+//#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
+//Solution
+
+//#4 Turn the trapped 3 number into: [3]
+const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
+//Solution
+
+//#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
+const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
+//Solution:
+
+//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+//Solution
+
+//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+//Solution
+updatedUsersArray = usersArray.map((user) => [user[0], user[1] * 2])
+
+//#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+//Solution
