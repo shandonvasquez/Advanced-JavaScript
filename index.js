09117631edcfc93d9862904ac429c6f182302afe
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-21 17:38:09 -0600

    Javascript Changes

--- index.js
+++ index.js
@@ -82,13 +82,15 @@ const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
 //
 console.log(trapped.flat(Infinity));
 
+
 //#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
 const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
 //Solution:
-
+console.log(userEmail3.trimStart().trimEnd());
 //#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
 const users = { user1: 18273, user2: 92833, user3: 90315 }
 //Solution
+console.log(Object.entries(users));
 
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
 //Solution
@@ -96,3 +98,6 @@ updatedUsersArray = usersArray.map((user) => [user[0], user[1] * 2])
 
 //#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
 //Solution
+
+console.log(Object.fromEntries(updatedUsersArray));
+
