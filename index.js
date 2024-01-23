194d025f2e4fed1b042b0dc8dfbfcc984e0b311f
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-22 21:59:36 -0600

    Javascript Changes

--- index.js
+++ index.js
@@ -181,4 +181,37 @@
 // }
 
 
+// Exercise 3: Clean up this code using optional chaining
+let will_pokemon = {
+    pikachu: {
+        species: 'Mouse Pokemon',
+        height: 0.4,
+        weight: 6,
+        power: 'lightning',
+        friend: { 
+            charizard: {
+                species: 'Dragon Pokemon',
+                height: 1.7,
+                weight: 90.5,
+                power: 'fire'
+            }
+        }
+    }
+}
+
+let andrei_pokemon = {
+    raichu: {
+        species: 'Mouse Pokemon',
+        height: 0.8,
+        weight: 30,
+        power: ''
+    }
+}
+
+if (andrei_pokemon?.raichu && will_pokemon?.pikachu?.friend?.charizard) {
+  console.log('fight!');
+} else {
+  console.log('walk away...');
+}
+
 
