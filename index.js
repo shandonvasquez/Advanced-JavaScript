43f10eded4cf6b6e69d2e59b114add68a96f0b0d
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-01-27 17:20:11 -0600

    Javascript Changes

--- index.js
+++ index.js
@@ -272,4 +272,7 @@ const ztmMonsters = [
     console.log(withMonsters)
 
 
-    .
+//Debugging with VS Code
+// it is the art of finding and fixing errors in your code and know how it works and acts
+
+
